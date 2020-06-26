71be271d8b7d3e1c1621fad5e13b867b2359bcce
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-06-26 15:14:57 +0200

    UserInterface

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -7,12 +7,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccountController extends AbstractController
 {
-    /**
-     * @Route("/account", name="account")
-     */
     public function index()
     {
         return $this->render('account/index.html.twig', [
+            'controller_name' => 'AccountController',
+        ]);
+    }
+
+    public function connexion()
+    {
+        return $this->render('connexion/index.html.twig', [
             'controller_name' => 'AccountController',
         ]);
     }

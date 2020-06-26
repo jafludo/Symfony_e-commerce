5ea08bd60ed352708bba45472a2e0e2730a702aa
Ludovic Jaffrezou <jaf.ludo@hotmail.fr>
2020-06-26 11:11:18 +0200

    Mon premier commit

---
+++ src/Entity/Application.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApplicationRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ApplicationRepository::class)
+ */
+class Application
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+    * @ORM\ManyToOne(targetEntity="App\Entity\Advert", inversedBy="applications")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $advert;
+
+    public function __construct() {
+        $this->date = new \Datetime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): self
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function setAdvert(Advert $advert)   {
+        $this->advert = $advert;
+        return $this;
+    }
+
+    public function getAdvert() {
+        return $this->advert;
+    }
+}
